c87a69477d432f9b5c43d7a380e66bf938a23700
Fepi <fpfarina@hotmail.com>
2017-07-24 15:26:22 -0300

    Bug corrected, when drawing map

--- src/components/views/NewTrip.vue
+++ src/components/views/NewTrip.vue
@@ -235,6 +235,7 @@ export default {
             cars: 'cars/cars'
         }),
         distanceString () {
+            console.log('distance');
             return Math.floor(this.trip.distance / 1000) + ' Km';
         },
         estimatedTimeString () {
@@ -369,14 +370,15 @@ export default {
 
         calcRoute () {
             for (let i = 0; i < this.points.length; i++) {
+                console.log (this.points);
                 if (!this.points[i].name) {
                     return;
                 }
             }
 
             this.directionsService.route({
-                origin: this.points[0].name,
-                destination: this.points[this.points.length - 1].name,
+                origin: this.points[0].location,
+                destination: this.points[this.points.length - 1].location,
                 travelMode: 'DRIVING'
             }, (response, status) => {
                 if (status === 'OK') {
@@ -395,9 +397,10 @@ export default {
                         totalDuration += legs[i].duration.value;
                     }
                     this.trip.distance = totalDistance;
-                    this.duration = totalDuration;
-                    this.co2 = totalDistance * 0.15; /* distancia por 0.15 kilos co2 en promedio por KM recorrido  */
+                    this.trip.duration = totalDuration;
+                    this.trip.co2 = totalDistance * 0.15; /* distancia por 0.15 kilos co2 en promedio por KM recorrido  */
                 } else {
+                    console.log(this.points[0].name, this.points[this.points.length - 1].name, 'DRIVING');
                     console.log('Directions request failed due to ' + status);
                 }
             });
