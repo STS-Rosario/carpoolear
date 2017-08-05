c863634576c4955a3bd122eeb45aee66641d8167
Fepi <fpfarina@hotmail.com>
2017-08-05 00:18:41 -0300

    friendrequestcard component

--- src/components/sections/FriendRequestCard.vue
+++ src/components/sections/FriendRequestCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-lg-6 col-md-8 col-sm-12">
-    <div class="friend-card rate-pending_component clearfix" > 
+    <div class="friend-card clearfix" > 
       <template v-if="user">
         <div class="rate-pending_photo">
             <router-link :to="{name: 'profile', params: {id: user.id, userProfile: user}}">
@@ -8,11 +8,9 @@
             </router-link>
         </div>
         <div class="rate-pending-message">
-          <div>
             <strong>{{ user.name }}</strong>
-            <slot></slot>
-          </div>
         </div>
+        <slot></slot>
       </template>
     </div>   
 
@@ -36,8 +34,13 @@ export default {
 </script>
 
 <style scoped>
+  .form-group {
+    width: 100%;
+  }
   .friend-card {
-    padding: 1em;
+    padding: .6em;
+    vertical-align: middle;
+    line-height: 5em;
   }
   .friend-card .trip_driver_img {
     width: 5em;
@@ -46,9 +49,24 @@ export default {
   .rate-pending_photo {
     margin-top: .2em;
     width: 6em;
+    float: left;
+    height: 5.5em;
+  }
+  .delete-friend {
+    width: 1.6em;
+    float: left;
+  }
+  .delete-friend i {
+    font-size: 20px;
+    cursor: pointer;
   }
   .rate-pending-message {
     margin-top: 0;
+    float: left;
+    width: calc(100% - 7.6em);
+    padding-left: .4em;
+    white-space: nowrap;
+    word-break: normal;
   }
   .pending-buttons button {
     width: calc(50% - .3em);
@@ -59,9 +77,15 @@ export default {
   .pending-buttons button:first-child {
     margin-right: .3em;
   }
-  #friends-list .col-sm-12 {
-     padding: 0;
+  #friends-list > div {
+    padding: 0;
   }
+  @media only screen and (min-width: 782px) {
+    .friends-list .col-sm-12 {
+      padding: 0;
+    }
+  }
+
   @media only screen and (min-width: 992px) {
     .btn-primary {
         font-size: 11px;
@@ -74,5 +98,18 @@ export default {
       .pending-buttons .btn-accept-request {
           padding: .92em;
       }
+    }
+    @media only screen and (max-width: 350px) {
+        .rate-pending_photo {
+            display: block;
+        }
+    }
+    @media only screen and (max-width: 300px) {
+        .rate-pending_photo {
+            display: none;
+        }
+        .rate-pending-message {
+            width: calc(100% - 1.6em);
+        }
     }
 </style>
