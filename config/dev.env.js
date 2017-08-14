c813727ad901d89c57d311cb8e627e53fe0d6666
Mariano Botta <marianoabotta@gmail.com>
2017-08-14 19:17:58 -0300

    Fix update trip with passenger

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"https://carpoolear.com.ar"',
+  API_URL: '"http://carpoolear.192.168.1.10.nip.io"',
   MAPS_API: '"AIzaSyBlRfNi2qDcy_zwjR53VVOyD-csjYSq4Qo"',
   FACEBOOK_API: '"147151221990591"'
 })
