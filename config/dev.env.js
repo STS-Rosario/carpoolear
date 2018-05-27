3904bc17ae129438dd7b57309605cd6002690d4b
Pablo Botta <pabloluisbotta@gmail.com>
2018-05-27 15:50:20 -0300

    Suscription ready-for production

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://localhost:8080"',
+  API_URL: '"https://carpoolear.com.ar"',
   MAPS_API: '"AIzaSyBlRfNi2qDcy_zwjR53VVOyD-csjYSq4Qo"',
   FACEBOOK_API: '"147151221990591"'
 })
