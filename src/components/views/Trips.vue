2835aea6fb9ba747b7852734bed57f7154cfba1f
Iago QUintero <iagoquintero00@gmail.com>
2019-10-04 12:00:39 -0300

    traducciones

--- src/components/views/Trips.vue
+++ src/components/views/Trips.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="trips container" :class="!user ? 'not-logged' : ''">
         <div class="trips_title" v-show="!isMobile">
-            <h1>Buscá con quién compartir tu próximo viaje!</h1>
-            <h3>¡Elegí fecha, origen o destino y encontralo!</h3>
+            <h1> {{ $t('buscaConQuien') }} </h1>
+            <h3>{{ $t('elegiDatos') }}</h3>
         </div>
         <template v-if="appConfig && appConfig.banner && appConfig.banner.url">
             <a :href="appConfig.banner.url" target="_blank" class="banner">
@@ -10,16 +10,16 @@
             </a>
         </template>
         <div v-show="!user && isMobile">
-            <router-link :to="{name: 'login'}" class="login_usuario"> Ingresá con tu usuario o registrate <span class='underline'>aquí</span> para comenzar a Carpoolear!</router-link>
+            <router-link :to="{name: 'login'}" class="login_usuario"> {{ $t('ingresaORegistrate') }} <span class='underline'> {{ $t('aqui') }} </span> {{ $t('paraComenzar') }} </router-link>
         </div>
         <SearchBox :params="searchParams" v-on:trip-search="research" v-show="!isMobile || lookSearch" ref="searchBox"></SearchBox>
         <Loading :data="trips" v-if="showingTrips">
             <div class="trips-list row">
                 <modal :name="'modal'" v-if="showModal" @close="showModal = false" :title="'Test'" :body="'Body'">
                     <h3 slot="header">
-                        <span>Doná a Carpoolear</span>
+                        <span> {{ $t('donaA') }}  </span>
                         <br class="hidden-sm hidden-md hidden-lg">
-                        <small>un proyecto de </small>
+                        <small> {{ $t('proyectoDe') }} </small>
                         <img width="90" alt="STS Rosario" src="https://carpoolear.com.ar/img/logo_sts_nuevo_color.png">
                     </h3>
                     <div slot="body" class="donation">
@@ -43,8 +43,8 @@
                             </label>
                         </div>
                         <div>
-                            <button class="btn btn-success btn-unica-vez" @click="onDonateOnceTime">ÚNICA VEZ</button>
-                            <button class="btn btn-info btn-mensualmente" @click="onDonateMonthly">MENSUAL <br />(cancelá cuando quieras)</button>
+                            <button class="btn btn-success btn-unica-vez" @click="onDonateOnceTime"> {{ $t('unicaVez') }} </button>
+                            <button class="btn btn-info btn-mensualmente" @click="onDonateMonthly">{{ $t('MENSUAL') }} <br />( {{ $t('cancelaCuando') }})</button>
                         </div>
                     </div>
                 </modal>
@@ -53,17 +53,17 @@
                         <div class="panel panel-default panel-donar" v-if="((index + parseFloat(appConfig.donation.trips_offset))  % parseFloat(appConfig.donation.trips_count) === 0)">
                             <div class="panel-body">
                                 <button class="btn btn-success pull-right btn-donar" @click="onDonate">Donar</button>
-                                <h2>Ayudanos a seguir siendo una plataforma abierta, colaborativa y sin fines de lucro</h2>
+                                <h2> {{ $t('ayudanos') }} </h2>
 
                                 <a href="/donar" target="_blank" v-on:click.prevent="onOpenLink('https://carpoolear.com.ar/donar?u=' + user.id)">
-                                    Por qué donar a Carpoolear
+                                    {{ $t('porQueDonar') }}
                                 </a>
                             </div>
                         </div>
                     </template>
                     <template v-if="isComplementary(trip, searchParams, index)">
                         <div class="trip-complementary">
-                            <h2>Resultados cercanos</h2>
+                            <h2> {{ $t('resultadosCercanos') }} </h2>
                         </div>
                     </template>
                     <Trip :trip="trip" :user="user"></Trip>
@@ -73,8 +73,8 @@
                 <p class="alert alert-warning"  role="alert"  :class="isMobile ? 'mobile-alert' : ''" v-if="resultaOfSearch && !alreadySubscribe">
                     <!-- <span class="sentence">¡Ups! No hay viajes con los criterios indicados en la búsqueda.</span> -->
                     <span class="sentence">
-                        <strong :class="isMobile ? 'sentence' : ''">Ahora podés suscribirte para que te avisemos cuando haya un nuevo viaje que concuerde con lo que estas buscando.</strong>
-                        <button class="btn btn-primary" v-if="user && !searchParams.data.is_passenger" @click="subscribeSearch" >Crear Alerta</button>
+                        <strong :class="isMobile ? 'sentence' : ''"> {{ $t('podesSubscribirte') }}</strong>
+                        <button class="btn btn-primary" v-if="user && !searchParams.data.is_passenger" @click="subscribeSearch" > {{ $t('crearAlerta') }}</button>
                     </span>
                 </p>
             </div>
@@ -85,18 +85,18 @@
             -->
             <div v-if="runningSearch" class="more-trips-loading">
                 <img src="https://carpoolear.com.ar/static/img/loader.gif" alt="" class="ajax-loader" />
-                Cargando más resultados
+                {{ $t('cargandoMasResultados') }}
             </div>
             <p slot="no-data" class="alert alert-warning"  role="alert"  :class="isMobile ? 'mobile-alert' : ''">
-                <span class="sentence">¡Ups! No hay viajes con los criterios indicados en la búsqueda.</span>
+                <span class="sentence">{{ $t('noHayViajes') }}</span>
                 <span class="sentence" v-if="!alreadySubscribe">
-                    <strong :class="isMobile ? 'sentence' : ''">Ahora podés suscribirte para que te avisemos cuando haya un nuevo viaje que concuerde con lo que estas buscando.</strong>
-                    <button class="btn btn-primary" v-if="user" @click="subscribeSearch" >Crear Alerta</button>
+                    <strong :class="isMobile ? 'sentence' : ''"> {{ $t('subscribirteAViajes') }}</strong>
+                    <button class="btn btn-primary" v-if="user" @click="subscribeSearch" >{{ $t('crearAlerta') }}</button>
                 </span>
             </p>
             <p slot="loading" class="alert alert-info" role="alert">
                 <img src="https://carpoolear.com.ar/static/img/loader.gif" alt="" class="ajax-loader" />
-                Cargando viajes ...
+                {{ $t('cargandoViajes') }}
             </p>
         </Loading>
     </div>
