103938f3090e1f1498cb10cabfffe1d90bdae55f
Iago QUintero <iagoquintero00@gmail.com>
2019-11-20 11:22:23 -0300

    Fix to title

--- src/store/modules/actionbars.js
+++ src/store/modules/actionbars.js
@@ -89,10 +89,11 @@ const actions = {
     setTitle (store, title = '') {
         let getters = globalStore.getters;
         let config = getters['auth/appConfig'];
-        let appName = config ? config.app_name : process.env.TARGET_APP;
+        let appName = config ? config.name_app : process.env.TARGET_APP;
         if (appName && appName.length) {
             appName = appName.charAt(0).toUpperCase() + appName.slice(1);
         }
+        console.log('APPNAME', appName);
         store.commit(types.HEADER_SET_TITLE, title);
         if (document) {
             document.title = title + (title !== appName ? (' - ' + appName) : '');
