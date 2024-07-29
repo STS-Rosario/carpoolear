f0dd0940c1bdadb57142100cb91fc76c27bb9031
Pablo Botta <pabloluisbotta@gmail.com>
2024-07-29 14:46:31 -0300

    recaptcha value

--- config/prod.carpoolear.env.js
+++ config/prod.carpoolear.env.js
@@ -5,7 +5,7 @@ config = {
   // API_URL: '"http://carpoolear.127.0.0.1.nip.io"',
   MAPS_API: '"AIzaSyBlRfNi2qDcy_zwjR53VVOyD-csjYSq4Qo"',
   FACEBOOK_API: '"147151221990591"',
-  RECAPTCHA_SITE_KEY: '"6LeVu34pAAAAAMpDut-0e0Wahe2vy1amM5MpANQz"',
+  RECAPTCHA_SITE_KEY: '"6LdoUqQpAAAAAJXZvlOk47gLqTapZ0gErj4KoFj_"',
   TARGET_APP: '"carpoolear"'
 
 }
