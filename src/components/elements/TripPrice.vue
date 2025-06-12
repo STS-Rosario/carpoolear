eb481ece8d12eb7ee437df0cb5f02b116e732d1f
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2025-06-11 22:30:21 -0300

    fix: TripPrice

--- src/components/elements/TripPrice.vue
+++ src/components/elements/TripPrice.vue
@@ -1,4 +1,5 @@
 <template>
+    
     <div
         class="trip-seats"
         v-if="tripCardTheme === 'light' || !trip.is_passenger"
@@ -9,6 +10,7 @@
             >
                 Precio asiento: <span>{{ $n(trip.seat_price_cents / 100, 'currency') }}</span>
             </div>
+            <span v-if="!trip.owner">Precio recomendado: <span>{{ $n(recommendedPricePerSeat, 'currency') }}</span></span>
         </div>
         <div v-if="tripCardTheme !== 'light'" style="height: 3.5em"></div>
         <template v-else>
@@ -28,8 +30,38 @@ export default {
     computed: {
         ...mapGetters({
             trip: 'trips/currentTrip',
-            tripCardTheme: 'auth/tripCardTheme'
-        })
+            tripCardTheme: 'auth/tripCardTheme',
+            user: 'auth/user',
+            config: 'auth/appConfig',
+        }),
+        owner() {
+            console.log('this.trip', this.trip);
+            console.log('this.user', this.user);
+            console.log('this.trip.user.id', this.trip.user.id);
+            console.log('this.user.id', this.user.id);
+            return this.trip && this.user && this.user.id === this.trip.user.id;
+        },
+        recommendedPricePerSeat() {
+            console.log('this.trip', this.trip);
+            console.log('this.config',this.config);
+            
+            // Get configuration values
+            const kmPerLiter = this.config.module_max_price_kilometer_by_liter || 10; // km/l
+            const literPrice = this.config.module_max_price_fuel_price || 1500; // $/l
+            
+            // Convert distance from meters to kilometers
+            const distanceInKm = this.trip.distance / 1000;
+            
+            // Calculate total fuel cost in dollars
+            // (km / (km/l)) * ($/l) = l * ($/l) = $
+            const totalFuelCost = (distanceInKm / kmPerLiter) * literPrice;
+            
+            // Calculate price per seat (including driver)
+            // $ / (seats + 1) = $/seat
+            const recommendedPrice = totalFuelCost / (this.trip.total_seats + 1);
+            
+            return Math.round(recommendedPrice);
+        }
     },
     components: {
         SvgItem
