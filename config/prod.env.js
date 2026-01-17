d84b4304b730fa1a0f59200e14751c8eae8ee513
Ziad Jeeroburkhan <ziadjkhan@gmail.com>
2026-01-18 00:37:30 +0400

    Update production environment configuration
    Removed RECAPTCHA_SITE_KEY and Firebase parameters from production environment configuration.

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,19 +3,10 @@ config = {
     API_URL: '"https://carpoolear.com.ar"',
     MAPS_API: '"AIzaSyDz9fNRr5-nfCFN6ULDZaLt0xUckgKl3z4"',
     FACEBOOK_API: '"147151221990591"',
-    RECAPTCHA_SITE_KEY: '"6LcJyuoqAAAAAFycfjGz2xTiOjhNmWP_wYudIiJT"',
     TARGET_APP: '"carpoolear"'
 };
 
-config.FIREBASE_PARAMS = {
-    apiKey: '"AIzaSyCwecybmdmiWUehjZBT30RZXWKgiNnP7XI"',
-    authDomain: '"carpoolear-production.firebaseapp.com"',
-    projectId: '"carpoolear-production"',
-    storageBucket: '"carpoolear-production.firebasestorage.app"',
-    messagingSenderId: '"726271358034"',
-    appId: '"1:726271358034:web:f09d2d38fae5f185f01b8b"',
-    measurementId: '"G-V8ZHPQJ6HV"'
-};
+
 
 config.FIRABASE_VAPID_KEY =
     '"BGtEjSYLZ36eiHMUpeHzondfyJFGvCvpOXyvBpa21cqxBpfXlV3s1FOrfiRyxbgSpx3m3fDjBb-Eb7FR7dlqZ-k"';
