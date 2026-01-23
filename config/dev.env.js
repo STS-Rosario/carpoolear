f2bf54626288ce5f83269f6a02a50cd241455cbe
ZiadJ <ziadjkhan@gmail.com>
2026-01-23 19:20:05 +0400

    Missing /app

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
     // API_URL: '"https://carpoolear.com.ar"',
     API_URL: '"http://carpoolear_backend.test"',
-    WEB_URL: '"https://carpoolear.com.ar"',
+    WEB_URL: '"https://carpoolear.com.ar/app"',
     MAPS_API: '"AIzaSyBlRfNi2qDcy_zwjR53VVOyD-csjYSq4Qo"',
     FACEBOOK_API: '"147151221990591"',
     RECAPTCHA_SITE_KEY: '"6LeVu34pAAAAAMpDut-0e0Wahe2vy1amM5MpANQz"',
