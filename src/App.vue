353d2890f183fa6c902cc976269b2ea677d67b55
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-01-30 08:54:45 -0300

    fix: App version

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <!-- Custom Splash Screen -->
         <div v-if="showCustomSplash" class="custom-splash-screen">
             <img src="https://carpoolear.com.ar/app/static/img/splash-android-1280x1920.png" alt="Carpoolear" class="splash-image" />
-            <div class="splash-version">Version 85</div>
+            <div class="splash-version">Version 90</div>
         </div>
         
         <onBoarding key="1" v-if="onBoardingVisibility"></onBoarding>
