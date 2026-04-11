45f959b67a6aa5128cb75c9e309300e5ad08587b
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-04-11 17:03:57 -0300

    fix: migrate Vue.directive() to app.directive()
    Refactored directives/index.js to export registerDirectives(app) function.
    Called after createApp() in main.js for app-scoped registration.

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,4 +1,3 @@
-import Vue from 'vue';
 import autofocus from './autofocus';
 import Jump from '@movilizame/vue-jumper';
 import imgSrc from './imageSrc';
@@ -9,12 +8,14 @@ import { VueMaskDirective } from 'v-mask';
 import clickOutside from './clickOutside';
 import fancyCheckbox from './fancyCheckbox';
 
-Vue.directive('focus', autofocus);
-Vue.directive('jump', Jump);
-Vue.directive('imgSrc', imgSrc);
-Vue.directive('mask', VueMaskDirective);
-Vue.directive('numberMask', numberFormatter);
-Vue.directive('dateFormatter', dateFormatter);
-Vue.directive('debounceInput', debounceInput);
-Vue.directive('clickoutside', clickOutside);
-Vue.directive('fancycheckbox', fancyCheckbox);
+export function registerDirectives(app) {
+    app.directive('focus', autofocus);
+    app.directive('jump', Jump);
+    app.directive('imgSrc', imgSrc);
+    app.directive('mask', VueMaskDirective);
+    app.directive('numberMask', numberFormatter);
+    app.directive('dateFormatter', dateFormatter);
+    app.directive('debounceInput', debounceInput);
+    app.directive('clickoutside', clickOutside);
+    app.directive('fancycheckbox', fancyCheckbox);
+}
