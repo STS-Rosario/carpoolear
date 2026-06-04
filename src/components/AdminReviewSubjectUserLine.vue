8d4e476bf1ca444205efcbe99e8af5bdec31bfe3
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-04 14:40:48 -0300

    feat: add AdminReviewSubjectUserLine shared component
    GREEN: reusable user line with admin and public profile links.

---
+++ src/components/AdminReviewSubjectUserLine.vue
@@ -0,0 +1,34 @@
+<template>
+    <p><strong>{{ $t(labelKey) }}:</strong>&nbsp;
+        <router-link v-if="userId" :to="getAdminUserProfileRoute(userId)">
+            {{ userName }}
+        </router-link>
+        <template v-if="userId">&nbsp;(<router-link :to="{ name: 'profile', params: { id: userId } }" target="_blank">{{ $t('verPerfilPublico') }}</router-link>)</template>
+        <span v-else>{{ userName || $t('na') }}</span>
+    </p>
+</template>
+
+<script>
+import { getAdminUserProfileRoute } from '../utils/adminProfileRoute';
+
+export default {
+    name: 'AdminReviewSubjectUserLine',
+    props: {
+        labelKey: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: [Number, String],
+            default: null
+        },
+        userName: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        getAdminUserProfileRoute
+    }
+};
+</script>
