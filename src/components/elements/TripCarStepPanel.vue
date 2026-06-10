ba4e69a8f3c4e393903766a56bbf6e094f42ffe5
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-10 20:47:13 -0300

    feat: open TripCarsModal from car step editar autos link
    Replace inline patente editor on the wizard car step with a link that
    opens the shared cars modal.

--- src/components/elements/TripCarStepPanel.vue
+++ src/components/elements/TripCarStepPanel.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="trip-car-step-panel">
+        <label for="trip-car-select" class="trip-car-step-panel__label">
+            {{ $t('seleccionarAuto') }}
+            <button
+                type="button"
+                class="trip-car-step-panel__edit-link btn btn-link"
+                @click="$emit('edit-cars')"
+            >
+                {{ $t('editarAutosEnViaje') }}
+            </button>
+        </label>
         <select
             id="trip-car-select"
             class="form-control trip-car-step-panel__select"
@@ -20,76 +30,17 @@
         <span class="error" v-if="carSelectionError.state">
             {{ carSelectionError.message }}
         </span>
-
-        <button
-            type="button"
-            class="trip-car-step-panel__add-link"
-            @click="showEditor = !showEditor"
-        >
-            {{ showEditor ? $t('cerrar') : $t('agregarOtroAuto') }}
-        </button>
-
-        <div v-if="showEditor" class="trip-car-step-panel__editor">
-            <div
-                v-for="(entry, index) in editorRows"
-                :key="entry.id ? 'car-' + entry.id : 'car-new-' + index"
-                class="trip-car-step-panel__row"
-            >
-                <input
-                    maxlength="20"
-                    v-model="entry.patente"
-                    type="text"
-                    class="form-control"
-                    :placeholder="$t('patente')"
-                />
-                <button
-                    v-if="canShowRemoveCarRow(entry, editorRows.length)"
-                    type="button"
-                    class="btn btn-default"
-                    :aria-label="$t('eliminarAuto')"
-                    @click="removeEditorRow(index)"
-                >
-                    <i aria-hidden="true" class="fa fa-times"></i>
-                </button>
-            </div>
-            <button
-                type="button"
-                class="btn btn-default trip-car-step-panel__add-row"
-                @click="addEditorRow"
-            >
-                {{ $t('agregarOtroAuto') }}
-            </button>
-            <button
-                type="button"
-                class="btn btn-primary trip-car-step-panel__save"
-                :disabled="savingCars"
-                @click="saveCars"
-            >
-                <spinner v-if="savingCars" class="blue"></spinner>
-                <span v-else>{{ $t('guardar') }}</span>
-            </button>
-            <span class="error" v-if="editorError">{{ editorError }}</span>
-        </div>
     </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'pinia';
+import { mapState } from 'pinia';
 import { useCarsStore } from '../../stores/car';
 import { formatCarSelectLabel } from '../../utils/carFields.js';
-import {
-    buildPatenteRowsFromCars,
-    canShowRemoveCarRow,
-    activeCarsWithPlate
-} from '../../utils/userCars.js';
-import spinner from '../Spinner.vue';
+import { activeCarsWithPlate } from '../../utils/userCars.js';
 
 export default {
     name: 'trip-car-step-panel',
-
-    components: {
-        spinner
-    },
 
     props: {
         selectedCarId: {
@@ -102,16 +53,7 @@ export default {
         }
     },
 
-    emits: ['update:selectedCarId', 'cars-updated'],
-
-    data() {
-        return {
-            showEditor: false,
-            editorRows: [{ id: null, patente: '' }],
-            editorError: '',
-            savingCars: false
-        };
-    },
+    emits: ['update:selectedCarId', 'cars-updated', 'edit-cars'],
 
     computed: {
         ...mapState(useCarsStore, {
@@ -130,108 +72,25 @@ export default {
         }
     },
 
-    watch: {
-        cars: {
-            immediate: true,
-            handler() {
-                if (this.showEditor) {
-                    this.syncEditorFromStore();
-                }
-            }
-        }
-    },
-
     methods: {
-        ...mapActions(useCarsStore, {
-            carCreate: 'create',
-            carUpdate: 'update',
-            carIndex: 'index'
-        }),
-        canShowRemoveCarRow,
-        formatCarSelectLabel,
-        syncEditorFromStore() {
-            this.editorRows = buildPatenteRowsFromCars(this.cars);
-        },
-        addEditorRow() {
-            this.editorRows.push({ id: null, patente: '' });
-        },
-        removeEditorRow(index) {
-            this.editorRows.splice(index, 1);
-            if (this.editorRows.length === 0) {
-                this.editorRows.push({ id: null, patente: '' });
-            }
-        },
-        async saveCars() {
-            this.editorError = '';
-            const rows = this.editorRows
-                .map((entry) => ({
-                    id: entry.id,
-                    patente: (entry.patente || '').trim()
-                }))
-                .filter((entry) => entry.patente);
-
-            if (!rows.length) {
-                this.editorError = this.$t('olvidastePatente');
-                return;
-            }
-
-            this.savingCars = true;
-            try {
-                for (const row of rows) {
-                    if (row.id) {
-                        await this.carUpdate({ id: row.id, patente: row.patente });
-                    } else {
-                        const created = await this.carCreate({ patente: row.patente });
-                        if (created && created.id) {
-                            this.$emit('update:selectedCarId', created.id);
-                        }
-                    }
-                }
-                await this.carIndex();
-                this.$emit('cars-updated');
-                this.showEditor = false;
-            } catch {
-                this.editorError = this.$t('problemaAlCargarElViaje');
-            } finally {
-                this.savingCars = false;
-            }
-        }
+        formatCarSelectLabel
     }
 };
 </script>
 
 <style scoped>
-.trip-car-step-panel__select {
-    margin-bottom: 0.75rem;
-}
-
-.trip-car-step-panel__add-link {
+.trip-car-step-panel__label {
     display: block;
-    margin: 0 auto 1rem;
-    background: none;
-    border: none;
-    color: inherit;
-    text-decoration: underline;
-    cursor: pointer;
-}
-
-.trip-car-step-panel__editor {
-    margin-top: 0.5rem;
-    padding-top: 0.75rem;
-    border-top: 1px solid #eee;
-}
-
-.trip-car-step-panel__row {
-    display: flex;
-    gap: 0.5rem;
     margin-bottom: 0.5rem;
 }
 
-.trip-car-step-panel__add-row {
-    margin-bottom: 0.75rem;
+.trip-car-step-panel__edit-link {
+    font-size: inherit;
+    padding: 0;
+    vertical-align: baseline;
 }
 
-.trip-car-step-panel__save {
-    min-width: 120px;
+.trip-car-step-panel__select {
+    margin-bottom: 0.75rem;
 }
 </style>
