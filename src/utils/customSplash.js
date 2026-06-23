2e3516beb2d728ea109ec04eb908795291d119b1
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-06-22 21:55:08 -0300

    chore: Upgrade to v3.3.2

--- src/utils/customSplash.js
+++ src/utils/customSplash.js
@@ -1,5 +1,5 @@
 export const CUSTOM_SPLASH_DISMISS_MS = 3000;
-export const SPLASH_WEB_BUILD_NUMBER = 124;
+export const SPLASH_WEB_BUILD_NUMBER = 125;
 
 export function formatSplashVersionText({
     version,
