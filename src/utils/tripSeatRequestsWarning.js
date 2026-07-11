46f099cbb8ea97abb83c3c8419ac6c2e13f5006c
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-07-11 19:42:53 -0300

    feat: seat request limit warning helpers

--- src/utils/tripSeatRequestsWarning.js
+++ src/utils/tripSeatRequestsWarning.js
@@ -1,3 +1,19 @@
-export function shouldShowTripSeatRequestsWarning(owner, passengerPendingCount) {
-    return Boolean(owner) && Number(passengerPendingCount) > 0;
+export function shouldShowTripSeatRequestsWarning(
+    owner,
+    passengerPendingCount,
+    seatRequestLimitReached = false
+) {
+    return (
+        Boolean(owner) &&
+        Number(passengerPendingCount) > 0 &&
+        !seatRequestLimitReached
+    );
+}
+
+export function shouldShowDriverSeatRequestLimitWarning(owner, trip) {
+    return Boolean(owner) && Boolean(trip?.seat_request_limit_reached);
+}
+
+export function shouldShowPassengerSeatRequestLimitMessage(owner, trip) {
+    return !owner && Boolean(trip?.seat_request_limit_reached);
 }
