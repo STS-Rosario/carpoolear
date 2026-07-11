296e50d3011406b118d6fd2435993bc1a295959d
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-07-10 21:07:44 -0300

    chore(release): update version to 3.3.5 across Android, iOS, and web components

--- src/utils/customSplash.js
+++ src/utils/customSplash.js
@@ -1,5 +1,5 @@
 export const CUSTOM_SPLASH_DISMISS_MS = 3000;
-export const SPLASH_WEB_BUILD_NUMBER = 130;
+export const SPLASH_WEB_BUILD_NUMBER = 135;
 
 export function formatSplashVersionText({
     version,
