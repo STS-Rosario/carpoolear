40880a69301f6841f6eda21ffc6de009e9775c55
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-07-31 15:43:12 -0300

    feat: vertically center trip card avatar beside driver rows
    Keep name and lugares on the first copy row and ratings with viajes on the second, with the avatar centered to their left.

--- src/components/elements/TripCardShell.vue
+++ src/components/elements/TripCardShell.vue
@@ -9,28 +9,30 @@
                 class="trip-card-shell__driver"
                 @click.stop="onProfileClick"
             >
-                <div class="trip-card-shell__primary">
-                    <div
-                        class="trip-card-shell__avatar circle-box"
-                        v-imgSrc:profile="avatarImage"
-                    ></div>
-                    <div class="trip-card-shell__name">
-                        <UserNameWithBadge :user="user" />
+                <div
+                    class="trip-card-shell__avatar circle-box"
+                    v-imgSrc:profile="avatarImage"
+                ></div>
+                <div class="trip-card-shell__copy">
+                    <div class="trip-card-shell__primary">
+                        <div class="trip-card-shell__name">
+                            <UserNameWithBadge :user="user" />
+                        </div>
+                        <div
+                            v-if="showSeatsPill"
+                            class="trip-card-shell__seats"
+                            :class="'trip-card-shell__seats--' + seatsTone"
+                        >
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            {{ seatsLabel }}
+                        </div>
                     </div>
-                    <div
-                        v-if="showSeatsPill"
-                        class="trip-card-shell__seats"
-                        :class="'trip-card-shell__seats--' + seatsTone"
-                    >
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        {{ seatsLabel }}
+                    <div class="trip-card-shell__meta">
+                        <UserRatingsCounts :ratings="ratings" />
+                        <span v-if="tripsCountLabel" class="trip-card-shell__trips">
+                            | {{ tripsCountLabel }}
+                        </span>
                     </div>
-                </div>
-                <div class="trip-card-shell__meta">
-                    <UserRatingsCounts :ratings="ratings" />
-                    <span v-if="tripsCountLabel" class="trip-card-shell__trips">
-                        | {{ tripsCountLabel }}
-                    </span>
                 </div>
             </div>
             <div
