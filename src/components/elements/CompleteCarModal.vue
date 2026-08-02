cb63638bc949a083a1b7869487747d4af2b98c3c
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-08-01 22:55:27 -0300

    feat: reuse CarForm in CompleteCarModal with disabled patente

--- src/components/elements/CompleteCarModal.vue
+++ src/components/elements/CompleteCarModal.vue
@@ -5,80 +5,16 @@
         </template>
         <template #body>
             <div class="complete-car-modal">
-            <p>{{ $t('completarDatosAutoDescripcion') }}</p>
-            <div class="form-group">
-                <label>{{ $t('patente') }}</label>
-                <input
-                    v-model="form.patente"
-                    type="text"
-                    class="form-control"
-                    disabled
+                <p>{{ $t('completarDatosAutoDescripcion') }}</p>
+                <CarForm
+                    :entry="form"
+                    :catalog-brands="catalogBrands"
+                    :catalog-models="catalogModels"
+                    :catalog-colors="catalogColors"
+                    patente-disabled
+                    @brand-selection-change="onBrandSelectionChange"
                 />
-            </div>
-            <div class="form-group">
-                <label>{{ $t('marca') }}</label>
-                <CatalogCombobox
-                    v-model="form.brandSelection"
-                    :options="catalogBrands"
-                    :placeholder="$t('buscarMarca')"
-                    :other-label="$t('marcaOtro')"
-                    @update:model-value="onBrandSelectionChange"
-                    @other-selected="onBrandSelectionChange('other')"
-                />
-            </div>
-            <div v-if="form.brandSelection === 'other'" class="form-group">
-                <input
-                    v-model="form.brand_other"
-                    type="text"
-                    class="form-control"
-                    :placeholder="$t('marcaOtroPlaceholder')"
-                />
-            </div>
-            <div class="form-group">
-                <label>{{ $t('modelo') }}</label>
-                <CatalogCombobox
-                    v-model="form.modelSelection"
-                    :options="catalogModels"
-                    :placeholder="$t('buscarModelo')"
-                    :other-label="$t('modeloOtro')"
-                    :disabled="!form.brandSelection"
-                    @update:model-value="onModelSelectionChange"
-                    @other-selected="onModelSelectionChange('other')"
-                />
-            </div>
-            <div v-if="form.modelSelection === 'other'" class="form-group">
-                <input
-                    v-model="form.model_other"
-                    type="text"
-                    class="form-control"
-                    :placeholder="$t('modeloOtroPlaceholder')"
-                />
-            </div>
-            <div class="form-group">
-                <label>{{ $t('anio') }}</label>
-                <input
-                    v-model.number="form.year"
-                    type="number"
-                    class="form-control"
-                    :min="carYearMin"
-                    :max="carYearMax"
-                    :placeholder="$t('anioPlaceholder')"
-                />
-            </div>
-            <div class="form-group">
-                <label>{{ $t('color') }}</label>
-                <select v-model="form.car_color_id" class="form-control">
-                    <option :value="null">{{ $t('seleccionarColor') }}</option>
-                    <option
-                        v-for="color in catalogColors"
-                        :key="color.id"
-                        :value="color.id"
-                    >
-                        {{ color.name }}
-                    </option>
-                </select>
-            </div>
-            <p v-if="error" class="error">{{ error }}</p>
+                <p v-if="error" class="error">{{ error }}</p>
             </div>
         </template>
         <template #footer>
@@ -96,23 +32,21 @@
 
 <script>
 import modal from '../Modal';
-import CatalogCombobox from './CatalogCombobox.vue';
+import CarForm from './CarForm.vue';
 import AppButton from '../ui/AppButton.vue';
 import { useCarCatalogStore } from '../../stores/carCatalog';
 import { useCarsStore } from '../../stores/car';
 import {
     buildCarFormFromCar,
     carPayloadFromForm,
-    CAR_YEAR_MIN,
-    CATALOG_OTHER_VALUE,
-    getCarYearMax
+    CATALOG_OTHER_VALUE
 } from '../../utils/carFields.js';
 
 export default {
     name: 'complete-car-modal',
     components: {
         modal,
-        CatalogCombobox,
+        CarForm,
         AppButton
     },
     props: {
@@ -126,14 +60,6 @@ export default {
         }
     },
     emits: ['close', 'saved'],
-    computed: {
-        carYearMin() {
-            return CAR_YEAR_MIN;
-        },
-        carYearMax() {
-            return getCarYearMax();
-        }
-    },
     data() {
         return {
             form: buildCarFormFromCar(null),
@@ -180,15 +106,6 @@ export default {
                 this.catalogModels = [];
             }
         },
-        onModelSelectionChange(value) {
-            this.form.modelSelection = value;
-            if (value === CATALOG_OTHER_VALUE) {
-                this.form.car_model_id = null;
-                return;
-            }
-            this.form.model_other = '';
-            this.form.car_model_id = value;
-        },
         onClose() {
             this.$emit('close');
         },
@@ -212,14 +129,3 @@ export default {
     }
 };
 </script>
-
-<style scoped>
-.complete-car-modal :deep(.form-control) {
-    color: #333;
-}
-
-.complete-car-modal :deep(select.form-control option) {
-    color: #333;
-    background: #fff;
-}
-</style>
