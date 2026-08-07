1117b70a0b34a4434c597d908ffdef211ff035ea
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-08-07 10:48:48 -0300

    feat: add Firebase Messaging plugin and configure iOS Firebase

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Capacitor
 import UserNotifications
+import FirebaseCore
 
 
 @UIApplicationMain
@@ -11,11 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        // Initialize Firebase
-        // FirebaseApp.configure()
-
-        // Configure push notifications
-        // UNUserNotificationCenter.current().delegate = self
+        FirebaseApp.configure()
 
         return true
     }
