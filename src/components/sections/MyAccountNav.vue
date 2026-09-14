0c12e56a75862d24a17b6057fbb2089aebd1b85d
Gonzalo González Mora <gonzalo@gonzalezmora.com>
2026-09-13 23:13:43 -0300

    feat: show splash version below delete account in menus
    Render the same version string as the splash screen in the desktop
    sidebar and mobile account menu, centered in grey below Eliminar cuenta.

--- src/components/sections/MyAccountNav.vue
+++ src/components/sections/MyAccountNav.vue
@@ -94,14 +94,22 @@
                 <i class="fa fa-trash-o" aria-hidden="true"></i>
                 {{ $t('eliminarCuenta') }}
             </router-link>
+            <p class="my-account-nav__version">{{ appVersionText }}</p>
         </div>
     </aside>
 </template>
 
 <script>
 import { mapState } from 'pinia';
+import { Capacitor } from '@capacitor/core';
 import { useAuthStore } from '../../stores/auth';
 import { useDeviceStore } from '../../stores/device';
+import { useRootStore } from '../../stores/root';
+import {
+    formatSplashVersionText,
+    resolveSplashVersion,
+    SPLASH_WEB_BUILD_NUMBER
+} from '../../utils/customSplash';
 import {
     DESKTOP_DELETE_ACCOUNT_ROUTE,
     getMyAccountDesktopExpandedSection,
@@ -143,6 +151,17 @@ export default {
         },
         deleteAccountRoute() {
             return DESKTOP_DELETE_ACCOUNT_ROUTE;
+        },
+        appVersionText() {
+            return formatSplashVersionText({
+                version: resolveSplashVersion({
+                    appVersionInfo: useRootStore().appVersionInfo,
+                    windowAppVersion:
+                        typeof window !== 'undefined' ? window.appVersion : null
+                }),
+                isNativePlatform: Capacitor.isNativePlatform(),
+                webBuildNumber: SPLASH_WEB_BUILD_NUMBER
+            });
         }
     },
     watch: {
@@ -317,5 +336,11 @@ export default {
 .my-account-nav__delete:hover,
 .my-account-nav__delete:focus {
     text-decoration: underline;
+}
+.my-account-nav__version {
+    margin: 0.75rem 0 0;
+    text-align: center;
+    color: #999;
+    font-size: 0.75rem;
 }
 </style>
--- src/components/views/MyAccount.vue
+++ src/components/views/MyAccount.vue
@@ -116,13 +116,21 @@
             <i class="fa fa-trash-o" aria-hidden="true"></i>
             {{ $t('eliminarCuenta') }}
         </router-link>
+        <p class="my-account__version">{{ appVersionText }}</p>
     </div>
 </template>
 
 <script>
 import { mapState } from 'pinia';
+import { Capacitor } from '@capacitor/core';
 import { useAuthStore } from '../../stores/auth';
 import { useDeviceStore } from '../../stores/device';
+import { useRootStore } from '../../stores/root';
+import {
+    formatSplashVersionText,
+    resolveSplashVersion,
+    SPLASH_WEB_BUILD_NUMBER
+} from '../../utils/customSplash';
 import { userRatingsFromProfile } from '../../utils/tripRating';
 import { normalizeTripsCount } from '../../utils/profileMemberStats';
 import {
@@ -170,6 +178,17 @@ export default {
         },
         deleteAccountRoute() {
             return MOBILE_DELETE_ACCOUNT_ROUTE;
+        },
+        appVersionText() {
+            return formatSplashVersionText({
+                version: resolveSplashVersion({
+                    appVersionInfo: useRootStore().appVersionInfo,
+                    windowAppVersion:
+                        typeof window !== 'undefined' ? window.appVersion : null
+                }),
+                isNativePlatform: Capacitor.isNativePlatform(),
+                webBuildNumber: SPLASH_WEB_BUILD_NUMBER
+            });
         }
     },
     methods: {
@@ -372,6 +391,12 @@ export default {
 .my-account__delete:hover,
 .my-account__delete:focus {
     text-decoration: underline;
+}
+.my-account__version {
+    margin: 0.75rem 0 0;
+    text-align: center;
+    color: #999;
+    font-size: 0.75rem;
 }
 @media only screen and (min-width: 768px) {
     .my-account {
